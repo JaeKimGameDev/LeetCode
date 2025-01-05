fca48249c4b267d5a223b2dfdbb17f5b06381652
JaeKimGameDev <166836886+JaeKimGameDev@users.noreply.github.com>
2025-01-05 13:31:35 -0700

    question 1 and 2
    question 1 and 2 completed

---
+++ 1. Two Sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        firstNumCounter = -1
+        firstNum = None
+        secondNum = None
+        for i in range(len(nums), 0, -1):
+            firstNumCounter += 1
+            firstNum = nums[firstNumCounter]
+            secondNumCounter = firstNumCounter
+
+            for k in range(1, i):
+                secondNum = nums[firstNumCounter + k]
+
+                if (target == firstNum + secondNum):
+                    return firstNumCounter, firstNumCounter + k
+
+        return
---
+++ 2. Add Two Numbers.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        r1, r2 = [], []
+        while l1:
+            r1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            r2.append(l2.val)
+            l2 = l2.next
+        r1.reverse()
+        r2.reverse()
+
+        r1 = "".join(map(str, r1))
+        r2 = "".join(map(str, r2))
+        result = int(r1) + int(r2)
+        addedResult = list(map(int, str(result)))
+        addedResult.reverse()
+
+        head = ListNode(addedResult[0])
+        current = head
+        for i in range(1, len(addedResult)):
+            current.next = ListNode(addedResult[i])
+            current = current.next
+
+        return head
