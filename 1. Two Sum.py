8459d06b5eff039e9ca7d0dfcd9d34c5310e889d
JaeKimGameDev <166836886+JaeKimGameDev@users.noreply.github.com>
2025-01-13 13:27:33 -0700

    twosum update
    twosum update

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,6 +1,6 @@
 #https://leetcode.com/problems/two-sum/description/
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
         firstNumCounter = -1
         firstNum = None
         secondNum = None
@@ -16,3 +16,18 @@ class Solution:
                     return firstNumCounter, firstNumCounter + k
 
         return
+
+    def twoSumVer2(self, nums, target):
+        d = {}
+        for position, value in enumerate(nums):
+            y = target - value
+            if y in d:
+                return [d[y], position]
+            d[value] = position
+
+nums = [2,7,11,15]
+target = 9
+
+myInstance = Solution()
+result = myInstance.twoSumVer2(nums, target)
+print(result)
