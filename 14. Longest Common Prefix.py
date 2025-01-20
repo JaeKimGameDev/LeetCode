a4e664f85f49e3198a7e46ac1d9b8150266c4b68
JaeKimGameDev <jaethegamedev@gmail.com>
2025-01-20 13:18:10 -0700

    14 completed
    14 completed

---
+++ 14. Longest Common Prefix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        p_leng=0
+        shortestString = min(strs, key=len)
+        longestPrefix = strs[0][:len(shortestString)]
+        # print(shortestString)
+        p_leng = 0
+
+        for i in range(len(shortestString)):
+            for k in range(len(strs)):
+                if longestPrefix[i] != strs[k][i]:
+                    return longestPrefix[:p_leng]
+            p_leng+=1
+        print("longestPrefix", longestPrefix, "longestPrefix[:p_leng]", longestPrefix[:p_leng])
+
+        return longestPrefix[:p_leng]
+
